e15d88c207d09a6af3d01cf03f26f32afbaa1b91
Atte Saarinen <atte.saarinen@vincit.fi>
2015-03-18 01:11:45 +0200

    Sponsor logo carousel

--- client/navigation/navigation.js
+++ client/navigation/navigation.js
@@ -5,23 +5,48 @@
     $scope.navigationClosed = false;
 
     $scope.toggleNavigation = function () {
-        $scope.navigationClosed = !$scope.navigationClosed;
+      $scope.navigationClosed = !$scope.navigationClosed;
     };
 
     var mobileView = 750;
 
     $scope.getWidth = function () {
-        return window.innerWidth;
+      return window.innerWidth;
     };
 
     $scope.$watch($scope.getWidth, function(newValue, oldValue) {
-        if (newValue <= mobileView) {
-            $scope.navigationClosed = true;
-        }
+      if (newValue <= mobileView) {
+        $scope.navigationClosed = true;
+      }
     });
 
     window.onresize = function () {
-        $scope.$apply();
+      $scope.$apply();
     };
+  })
+
+  .controller('SponsorController', function ($scope, $interval) {
+    var baseUrl = '/assets/';
+
+    $scope.logos = [
+      { name: 'vincit', url: baseUrl + 'vincit.png' },
+      { name: 'eatech', url: baseUrl + 'eatech.png' },
+      { name: 'wapice', url: baseUrl + 'wapice.png' },
+      { name: 'mfiles', url: baseUrl + 'mfiles.png' },
+    ];
+
+    
+    var x = Math.floor(Math.random() * $scope.logos.length);
+    $scope.showLogo = $scope.logos[x];
+
+    $interval(function () {
+      x++;
+      
+      if (x === $scope.logos.length) {
+        x = 0;
+      }
+
+      $scope.showLogo = $scope.logos[x];
+    }, 5000);
   });
 }(angular));
