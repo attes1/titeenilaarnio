c8d7d13b6507f154a0a07f1d023f86a8e88872f4
Dregu <dregu@dreg.us>
2015-03-18 12:51:25 +0200

    map works

--- client/map/map.js
+++ client/map/map.js
@@ -1,6 +1,6 @@
 (function (angular) {
   'use strict';
-  angular.module('titeenilaarnio.map', [ 'ui.router', 'titeenilaarnio.map', 'uiGmapgoogle-maps' ])
+  angular.module('titeenilaarnio.map', [ 'uiGmapgoogle-maps', 'ui.router', 'titeenilaarnio.map' ])
   .config(function ($stateProvider) {
     $stateProvider
     .state('titeenilaarnio.map', {
@@ -12,14 +12,14 @@
       }
     })
   }).controller('MapController', function($scope) {
-    $scope.map = { center: { latitude: 61.450650, longitude: 23.855030 }, zoom: 16 }
     $scope.options = { scrollwheel: true }
-    $scope.markers =
+    $scope.points =
     [{
       id: "tite",
       title: "TiTe",
       text: "Kaikille samat säännöt.",
       icon: "/assets/icons/tite.png",
+      show: false,
       coords: {
         latitude: 61.4498286,
         longitude: 23.8572004
@@ -30,6 +30,7 @@
       title: "Kisakylä",
       text: "Sattuu ja tapahtuu.",
       icon: "/assets/icons/skull33.png",
+      show: false,
       coords: {
         latitude: 61.4505921,
         longitude: 23.864358
@@ -40,6 +41,7 @@
       title: "Bommari",
       text: "Nuku hyvin.",
       icon: "/assets/icons/bed33.png",
+      show: false,
       coords: {
         latitude: 61.450872,
         longitude: 23.862116
@@ -50,6 +52,7 @@
       title: "Juvenes ravintola Edison",
       text: "",
       icon: "/assets/icons/restaurant1.png",
+      show: false,
       coords: {
         latitude: 61.450360,
         longitude: 23.859109
@@ -60,6 +63,7 @@
       title: "Pub Kultainen Apina",
       text: "",
       icon: "/assets/icons/beer34.png",
+      show: false,
       coords: {
         latitude: 61.451809,
         longitude: 23.847927
@@ -70,6 +74,7 @@
       title: "Pizzamestat",
       text: "",
       icon: "/assets/icons/pizza3.png",
+      show: false,
       coords: {
         latitude: 61.451553,
         longitude: 23.848402
@@ -80,6 +85,7 @@
       title: "Kauppa&Keskus Duo",
       text: "",
       icon: "/assets/icons/marker20.png",
+      show: false,
       coords: {
         latitude: 61.450929,
         longitude: 23.850892
@@ -90,6 +96,7 @@
       title: "Systembolaget",
       text: "Sauli Anto, Systembolaget ei.",
       icon: "/assets/icons/dark45.png",
+      show: false,
       coords: {
         latitude: 61.451236,
         longitude: 23.852270
@@ -100,11 +107,31 @@
       title: "Herwood Grilli",
       text: "",
       icon: "/assets/icons/fastfood1.png",
+      show: false,
       coords: {
         latitude: 61.450141,
         longitude: 23.849697
       }
     }
-    ];
+    ]
+    var showing = null;
+    $scope.map = { center: { latitude: 61.450650, longitude: 23.855030 }, zoom: 16, markers: $scope.points }
+    function onMarkerClicked(marker) {
+      if(showing){
+        var tohide = _.filter($scope.map.markers,function(x){if(x.id == showing) {return true} return false})[0]
+        tohide.show = false
+      }
+      _.each($scope.map.markers, function(tohide) {
+        tohide.show = false
+      })
+      showing = marker.id
+      marker.show = true
+      $scope.$apply()
+    }
+    _.each($scope.map.markers, function(amarker) {
+      amarker.onClicked = function() {
+        onMarkerClicked(amarker)
+      };
+    });
   })
 }(angular));
