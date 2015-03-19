81efb4a9dd52c8ebe55ea008a5c4753069f34df9
Atte Saarinen <atte.saarinen@vincit.fi>
2015-03-19 18:09:52 +0200

    Port as command line parameter (defaults to 9000)
    Syntax:
    node server.js [port]

--- server/server.js
+++ server/server.js
@@ -8,9 +8,14 @@
  *
 */
 
-var app   = require('./main/app.js'),
-    port  = app.get('port'),
-    log   = 'Listening on ' + app.get('base url') + ':' + port;
+var app   = require('./main/app.js');
+var port  = app.get('port');
+
+if (process.argv.length === 3) {
+  port = process.argv[2];
+}
+
+var log   = 'Listening on ' + app.get('base url') + ':' + port;
 
 app.listen(port);
 console.log(log);
