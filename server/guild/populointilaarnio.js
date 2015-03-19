809733501c9188bae166efc74c56b47e9d3e259d
Akseli Karvinen <kebbish@gmail.com>
2015-03-19 16:08:56 -0400

    populointi ja backia

---
+++ server/guild/populointilaarnio.js
@@ -0,0 +1,29 @@
+// run with "node populointilaarnio.js"
+"use strict";
+
+var mongoose = require('mongoose');
+var guildmodel = require('./guild_model.js');
+1
+var Guild = mongoose.model('guilds');
+
+var db = mongoose.connect('mongodb://localhost/titeenilaarnio');
+
+var guild = new Guild({
+	shortName: 'tite',
+	fullName: 'Tampeen TietoTeekkarikilta',
+	scores: 
+	{
+		score: 0,
+		competition: 'mic'
+
+	},
+	bonuscode: 'ebin'
+});
+
+guild.save(function (err) {
+	if (err) throw err;
+
+	console.log('Kilta:');
+	console.log(guild);
+	console.log('Created successfully.');
+});
