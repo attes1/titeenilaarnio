54c00b8ae51a6abef264f1d3c96ec3206c58735f
Atte Saarinen <atte.saarinen@vincit.fi>
2015-03-20 02:20:57 +0200

    Scoreboard

--- client/scoreboard/scoreboard.js
+++ client/scoreboard/scoreboard.js
@@ -7,9 +7,29 @@
       url: 'scoreboard',
       views: {
         'main@': {
-          templateUrl: 'scoreboard/scoreboard.html'
+          templateUrl: 'scoreboard/scoreboard.html',
+          controller: 'ScoreboardController'
         }
       }
+    });
+  })
+
+  .controller('ScoreboardController', function ($scope, $http) {
+    $scope.guilds = undefined;
+
+    $http.get('/api/guilds')
+      .success(function (response) {
+        $scope.guilds = response;
+        _.forEach($scope.guilds, function (g) {
+          g.total = 0;
+          g.total = _.reduce(g.scores, function (sum, value, key) {
+            if (key === 'score') {
+              sum = sum + value;
+            }
+
+            return sum;
+          }, 0);
+        });
     });
   });
 }(angular));
